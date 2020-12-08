48a6a60e2dd6a56901c1a8198194b54b8da64422
dingchaofa <571797089@qq.com>
2020-12-08 22:08:38 +0800

    optimized call

--- leetcode/2/2.go
+++ leetcode/2/2.go
@@ -50,31 +50,30 @@ func (l *ListNode) String() string {
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	var call int
 	var tail, head *ListNode
-	for l1 != nil || l2 != nil || call >0 {
-		if l1 == nil {
-			l1 = &ListNode{}
+	for l1 != nil || l2 != nil || call > 0 { // call只执行一次，call放在这里运算更加快速，如果不单独再if判断call
+		var n1, n2 int
+		if l1 != nil {
+			n1 = l1.Val
+			l1 = l1.Next
 		}
-		if l2 == nil {
-			l2 = &ListNode{}
+		if l2 != nil {
+			n2 = l2.Val
+			l2 = l2.Next
 		}
+		sum := n1 + n2 + call
 		if tail == nil {
 			head = &ListNode{ // 先固定头节点
-				Val: (l1.Val + l2.Val + call) % 10,
+				Val: sum % 10,
 			}
 			tail = head
 		} else {
 			tail.Next = &ListNode{
-				Val: (l1.Val + l2.Val + call) % 10,
+				Val: sum % 10,
 			}
 			tail = tail.Next // 每次移动尾节点
 		}
-		call = (l1.Val + l2.Val + call) / 10
-		if l1 != nil {
-			l1 = l1.Next
-		}
-		if l2 != nil {
-			l2 = l2.Next
-		}
+		call = sum / 10
 	}
+
 	return head
 }
