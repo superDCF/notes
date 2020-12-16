1bf4fe2b9ba34ea11d3f4185155b0697ab1b383f
dingchaofa <571797089@qq.com>
2020-12-17 00:04:43 +0800

    求余符号处理

--- leetcode/7/7.go
+++ leetcode/7/7.go
@@ -20,17 +20,10 @@ func main() {
 // 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
 // 123 -> 321
 func reverse(x int) int {
-	neg := x < 0
-	if neg {
-		x = -x
-	}
 	n := 0
-	for x > 0 {
+	for x != 0 {
 		n = n*10 + x%10
 		x = x / 10
-	}
-	if neg {
-		n = -n
 	}
 	if int(int32(n)) == n {
 		return n
