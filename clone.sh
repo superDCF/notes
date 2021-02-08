9dabb8be82b69280218ae725d1c9508657477e15
dingchaofa <571797089@qq.com>
2021-02-08 11:28:58 +0800

    clone all git repo shell

---
+++ clone.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+## login gitlab cookie
+# set -e
+page=1
+count=0
+gitlab=$2 # gitlab domain ex:code.hellotalk.com
+url="https://$gitlab/?non_archived=true&page=$page&sort=latest_activity_desc"
+response="$(curl -LSs -b "$1" $url)"
+declare -a dirs
+# while (test $page -lt 2) ;do
+while  [[ $response =~ class=\"project\" ]];do
+    list="$(echo "$response" | awk -F '"' '/class="project"/ {print $4}')"
+    echo "$list"
+    for str in $list;do
+        echo $str
+        dirs[$count]="$str"
+        ((count++))
+        echo $count
+    done
+    ((page++))
+    echo $page
+    url="https://$gitlab/?non_archived=true&page=$page&sort=latest_activity_desc"
+    echo $url
+    response="$(curl -LSs -b "$1" $url)"
+done
+echo ${dirs[@]}
+basepath=~/$gitlab # clone dir
+for dir in ${dirs[@]};do
+    echo $dir
+    name=${dir##/*/}
+    echo $name
+    subpath=${dir%/*}
+    echo $subpath
+    path=$basepath$subpath
+    echo $path
+    mkdir -p $path
+    cd $path
+    git clone git@$gitlab:$dir.git
+done
