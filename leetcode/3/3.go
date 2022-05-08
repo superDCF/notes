a777f114a72f0980faedf2af0ae8cd54840077da
dingchaofa2 <571797089@qq.com>
2022-05-08 19:05:03 +0800

    leetcode 3

--- leetcode/3/3.go
+++ leetcode/3/3.go
@@ -3,13 +3,11 @@ package main
 import "log"
 
 func main() {
-	log.Println(lengthOfLongestSubstring("dvdf")) // "pwwkew" // abcabcbb //"au"
-	s := "pwwkew11发"
-	log.Println(len(s), s[0], s[len(s)-1], s[len(s)-2], s[len(s)-3])
-	log.Printf("%T", s[len(s)-2])
-	// for i, v := range s {
-	// 	// log.Println(i, v)
-	// }
+	// log.Println(lengthOfLongestSubstring("dvdf")) // "pwwkew" // abcabcbb //"au"
+	// s := "pwwkew11发"
+	// log.Println(len(s), s[0], s[len(s)-1], s[len(s)-2], s[len(s)-3])
+	// log.Printf("%T", s[len(s)-2])
+	log.Println(lengthOfLongestSubstring2("a"))
 }
 
 // s是 由英文字母、数字、符号和空格组成，所以不用考虑多字节字符
@@ -33,24 +31,53 @@ func lengthOfLongestSubstring(s string) int {
 					start = i - 1
 				}
 				m[s[i]] = i
-				if i == len(s) - 1 { // 保证最后一个加入
+				if i == len(s)-1 { // 保证最后一个加入
 					index[i-start+1] = start
 				}
 			}
 
 		} else {
 			index[i-start] = start
-			m = map[byte]struct{}{
-				s[i]: struct{}{},
+			m = map[byte]int{
+				s[i]: i,
 			}
 			start = i
 		}
-		
+
 	}
 	maxLen := 1
 	for k := range index {
 		if k > maxLen {
 			maxLen = k
+		}
+	}
+	return maxLen
+}
+
+// 滑动窗口
+// abcabcbb
+// abcabcbbace
+func lengthOfLongestSubstring2(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	maxLen := 1
+	ret := string(s[0])
+	for i := 1; i < len(s); i++ {
+		existIndex := -1
+		for j := 0; j < len(ret); j++ {
+			if s[i] == ret[j] {
+				existIndex = j
+				break
+			}
+		}
+		if existIndex > -1 {
+			ret = ret[existIndex+1:] + string(s[i])
+		} else {
+			ret += string(s[i])
+		}
+		if len(ret) > maxLen {
+			maxLen = len(ret)
 		}
 	}
 	return maxLen
